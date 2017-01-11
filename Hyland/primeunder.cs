3eda1445b92dbb37c623c1c9f5b3c08b326ea874
Bryant <wbryantcampbell@gmail.com>
2017-01-11 13:18:33 -0500

    small fixes to hyland submission

--- Hyland/primeunder.cs
+++ Hyland/primeunder.cs
@@ -1,31 +1,33 @@
-// 1 and 0 are not prime we know this. 
-// We also know that 2 and 3 are prime
-//because they are only divisible by one
-//and themselves. 
-var primes = new List<int> { 2, 3 };
-//any even number above 2 is also out 
-//because we know that evens are divisible
-//by 2
-for (int i = 5; i <= 100; i+=2)
+static void prime()
 {
-    bool isPrime = true;
-    //we need to check each prime number already in the list
-    foreach (int prime in primes)
+    // 1 and 0 are not prime we know this. 
+    // We also know that 2 and 3 are prime
+    //because they are only divisible by one
+    //and themselves. 
+    var primes = new List<int> { 2, 3 };
+    //any even number above 2 is also out 
+    //because we know that evens are divisible
+    //by 2
+    for (int i = 5; i <= 100; i += 2)
     {
-        //if i is a factor of of any number in primes we set isPrime to false
-        if(i % prime == 0)
+        bool isPrime = true;
+        //we need to check each prime number already in the list
+        foreach (int prime in primes)
         {
-            isPrime = false;
+            //if i is a factor of of any number in primes we set isPrime to false
+            if (i % prime == 0)
+            {
+                isPrime = false;
+            }
+        }
+        //if isPrime remains true i is added to primes
+        if (isPrime)
+        {
+            primes.Add(i);
         }
     }
-    //if isPrime remains true i is added to primes
-    if (isPrime)
+    foreach (int prime in primes)
     {
-        primes.Add(i);
+        print(prime);
     }
 }
-foreach (int prime in primes)
-{
-    Console.Write($"{prime} ");
-}
-Console.ReadLine();
